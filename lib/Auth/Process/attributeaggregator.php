68771d5a2de6d8feb1ee824848f0b4dbd859d610
Uherkovich Péter <uherkovich.peter@gmail.com>
2026-01-01 21:13:11 +0100

    - fix autoload - PSR12

--- lib/Auth/Process/attributeaggregator.php
+++ lib/Auth/Process/attributeaggregator.php
@@ -1,4 +1,7 @@
 <?php
+
+declare(strict_types=1);
+
 /**
  * Attribute Aggregator Authentication Processing filter
  *
@@ -9,6 +12,7 @@
  * @package simpleSAMLphp
  * @version $Id$
  */
+
 namespace SimpleSAML\Module\attributeaggregator\Auth\Process;
 
 use SimpleSAML\Auth\ProcessingFilter;
@@ -38,7 +42,7 @@ class attributeaggregator extends ProcessingFilter
     /**
      *
      * attributeId, the key of the user in the AA. default is eduPersonPrincipalName
-     * @var unknown_type
+     * @var string
      */
     private $attributeId = "urn:oid:1.3.6.1.4.1.5923.1.1.1.6";
 
@@ -50,9 +54,9 @@ class attributeaggregator extends ProcessingFilter
     private $required = false;
 
     /**
-     * 
+     *
      * nameIdFormat, the format of the attributeId. Default is "urn:oasis:names:tc:SAML:2.0:nameid-format:persistent";
-     * @var unknown_type
+     * @var string
      */
     private $nameIdFormat = Constants::NAMEID_PERSISTENT;
 
@@ -84,7 +88,7 @@ class attributeaggregator extends ProcessingFilter
      *
      * @var string
      */
-    private $aaEndpoint='';
+    private $aaEndpoint = '';
 
     /**
      * The metadata of this SP
@@ -98,7 +102,7 @@ class attributeaggregator extends ProcessingFilter
      *
      * Validates and parses the configuration
      *
-     * @param array $config   Configuration information
+     * @param array $config Configuration information
      * @param mixed $reserved For future use
      */
     public function __construct($config, $reserved)
@@ -113,11 +117,11 @@ class attributeaggregator extends ProcessingFilter
         // so we need to take the path $state->authsourceid->authsource->metadata once
         // we have $state in ::process()
 
-        if (!empty($config["attributeId"])){
+        if (!empty($config["attributeId"])) {
             $this->attributeId = $config["attributeId"];
         }
-        
-        if (!empty($config["required"])){
+
+        if (!empty($config["required"])) {
             $this->required = $config["required"];
         }
 
@@ -125,32 +129,35 @@ class attributeaggregator extends ProcessingFilter
             $this->nameIdFormat = $config["nameIdFormat"];
         }
 
-        if (!empty($config["attributes"])){
-            if (! is_array($config["attributes"])) {
+        if (!empty($config["attributes"])) {
+            if (!is_array($config["attributes"])) {
                 throw new Exception("attributeaggregator: Invalid format of attributes array in the configuration");
             }
             foreach ($config["attributes"] as $attribute) {
-                if (! is_array($attribute)) {
+                if (!is_array($attribute)) {
                     throw new Exception("attributeaggregator: Invalid format of attributes array in the configuration");
                 }
                 if (array_key_exists("values", $attribute)) {
-                    if (! is_array($attribute["values"])) {
-                        throw new Exception("attributeaggregator: Invalid format of attributes array in the configuration");
-                    }    
-                }
-                if (array_key_exists('multiSource', $attribute)){
-                    if(! preg_match('/^(merge|keep|override)$/', $attribute['multiSource']))
+                    if (!is_array($attribute["values"])) {
                         throw new Exception(
-                            'attributeaggregator: Invalid multiSource value "'.
-                            $attribute['multiSource'].'" for '.key($attribute).
+                            "attributeaggregator: Invalid format of attributes array in the configuration"
+                        );
+                    }
+                }
+                if (array_key_exists('multiSource', $attribute)) {
+                    if (!preg_match('/^(merge|keep|override)$/', $attribute['multiSource'])) {
+                        throw new Exception(
+                            'attributeaggregator: Invalid multiSource value "' .
+                            $attribute['multiSource'] . '" for ' . key($attribute) .
                             '. Should be one of keep, merge or override.'
-                    );
+                        );
+                    }
                 }
             }
             $this->attributes = $config["attributes"];
         }
 
-        if (!empty($config["attributeNameFormat"])){
+        if (!empty($config["attributeNameFormat"])) {
             $this->attributeNameFormat = $config["attributeNameFormat"];
         }
 
@@ -161,28 +168,29 @@ class attributeaggregator extends ProcessingFilter
                     $metadata->getMetaData($this->entityId, 'attributeauthority-remote')
                 );
                 if ($this->aaMetadata->hasValue('AttributeService')) {
-                    foreach ($this->aaMetadata->getArray('AttributeService',array()) as $aa_endpoint) {
+                    foreach ($this->aaMetadata->getArray('AttributeService', array()) as $aa_endpoint) {
                         if ($aa_endpoint['Binding'] == Constants::BINDING_SOAP) {
                             $this->aaEndpoint = $aa_endpoint['Location'];
                             break;
                         }
                     }
                 }
-                if(empty($this->aaEndpoint)) {
-                    throw new \RuntimeException($this->entityId.' does not have a usable AttributeService endpoint in metadata'.
-                    ' with binding '. Constants::BINDING_SOAP);
+                if (empty($this->aaEndpoint)) {
+                    throw new \RuntimeException(
+                        $this->entityId . ' does not have a usable AttributeService endpoint in metadata' .
+                        ' with binding ' . Constants::BINDING_SOAP
+                    );
                 }
             } catch (\Exception $e) {
-                Logger::warning('Unable to perform attribute query, no metadata for '.$this->entityId);
+                Logger::warning('Unable to perform attribute query, no metadata for ' . $this->entityId);
                 if ($this->required) {
                     throw $e;
                 }
             }
-        }
-        else {
+        } else {
             throw new Exception(
-                    'attributeaggregator: AA entityId is not specified in the configuration.'
-                );
+                'attributeaggregator: AA entityId is not specified in the configuration.'
+            );
         }
     }
 
@@ -191,7 +199,7 @@ class attributeaggregator extends ProcessingFilter
      *
      * @param array &$state The state of the response.
      */
-    public function process(&$state)
+    public function process(&$state): void
     {
         assert('is_array($state)');
 
@@ -209,7 +217,7 @@ class attributeaggregator extends ProcessingFilter
             throw new Exception("Unable to access the auth source ID");
         }
         $authsource = Source::getById($state["saml:sp:State"]["saml:sp:AuthId"]);
-        // This must be instanceof SimpleSAML\Module\saml\Auth\Source\SP 
+        // This must be instanceof SimpleSAML\Module\saml\Auth\Source\SP
         if (!($authsource instanceof \SimpleSAML\Module\saml\Auth\Source\SP)) {
             throw new Exception("Auth source is not a SAML SP");
         }
@@ -219,53 +227,55 @@ class attributeaggregator extends ProcessingFilter
             // verify that we are having all the necessary information
             if (empty($state['Attributes'][$this->attributeId]) ||
                 count($state['Attributes'][$this->attributeId]) > 1) {
-                throw new \RuntimeException("Can't attempt attribute query, attribute ".
-                    $this->attributeId." is not present or has multiple values");
+                throw new \RuntimeException(
+                    "Can't attempt attribute query, attribute " .
+                    $this->attributeId . " is not present or has multiple values"
+                );
             }
 
             // build attribute query
             $query = new AttributeQuery();
             $query->setDestination($this->aaEndpoint);
             $query->setIssuer($this->selfMetadata->getValue('entityID'));
-            $nameid = NameID::fromArray (
-                array(
-                    'Value' => $state['Attributes'][$this->attributeId][0],
-                    'Format' => $this->nameIdFormat,
-                )
-            );
+            $nameid = new NameID();
+            $nameid->setValue($state['Attributes'][$this->attributeId][0]);
+            $nameid->setFormat($this->nameIdFormat);
             $query->setNameId($nameid);
             $query->setAttributeNameFormat($this->attributeNameFormat);
             $query->setAttributes($this->getRequestedAttributes()); // may be empty, then it's a noop
-            $query->setID(Random::generateID());
+            $randomUtil = new Random();
+            $query->setID($randomUtil->generateID());
             // TODO: should this call be made optional?
-            Message::addSign($this->selfMetadata,$this->aaMetadata,$query);
+            Message::addSign($this->selfMetadata, $this->aaMetadata, $query);
             // send attribute query
-            Logger::debug('Sending attribute query: '.var_export($query,true));
+            Logger::debug('Sending attribute query: ' . var_export($query, true));
             $binding = new SOAPClient();
-            $response = $binding->send($query,$this->selfMetadata,$this->aaMetadata);
+            $response = $binding->send($query, $this->selfMetadata, $this->aaMetadata);
 
             // verify result
-            Logger::debug('Received attribute response: '.var_export($response,true));
+            Logger::debug('Received attribute response: ' . var_export($response, true));
             if (!$response->isSuccess()) {
-                throw new \RuntimeException('Got a SAML error on attribute query ('.
-                    $response->getStatus()['Code'].')');
+                throw new \RuntimeException(
+                    'Got a SAML error on attribute query (' .
+                    $response->getStatus()['Code'] . ')'
+                );
             }
             // merge attributes
             $assertion = $response->getAssertions()[0]; // TODO Can there be more than 1?
             if (empty($assertion)) {
                 throw new \RuntimeException('Got an empty SAML Response');
-	    }
+            }
             $this->mergeAttributes($state, $assertion->getAttributes());
-        
         } catch (\Exception $e) {
-            Logger::info("Attribute query failed: ".$e->getMessage());
+            Logger::info("Attribute query failed: " . $e->getMessage());
             if ($this->required) {
                 throw $e;
             }
         }
     }
 
-    private function mergeAttributes(&$state, $attributes_from_aa) {
+    private function mergeAttributes(&$state, $attributes_from_aa)
+    {
         if (empty($attributes_from_aa)) {
             return;
         }
@@ -275,9 +285,9 @@ class attributeaggregator extends ProcessingFilter
             if (array_key_exists($name, $this->attributes) ||
                 array_key_exists('*', $this->attributes)) {
                 // Filter out values that don't match
-                $this->filter_attribute_values($name,$values);
+                $this->filter_attribute_values($name, $values);
                 if (empty($values)) {
-                    Logger::info("[attributeaggregator] No values left for attribute ".$name);
+                    Logger::info("[attributeaggregator] No values left for attribute " . $name);
                     continue;
                 }
 
@@ -291,26 +301,25 @@ class attributeaggregator extends ProcessingFilter
                     // Do we have a merge policy?
                     if (isset($mergePolicy)) {
                         switch ($mergePolicy) {
-                        case 'override':
-                            self::override_attribute_values($state,$name,$values);
-                            break;
-                        case 'keep':
-                            Logger::info('[attributeaggregator] Keeping attribute '.$name);
-                            continue 2;
-                            break;
-                        case 'merge':
-                            self::merge_attribute_values($state,$name,$values);
-                            break;
+                            case 'override':
+                                self::override_attribute_values($state, $name, $values);
+                                break;
+                            case 'keep':
+                                Logger::info('[attributeaggregator] Keeping attribute ' . $name);
+                                continue 2;
+                            case 'merge':
+                                self::merge_attribute_values($state, $name, $values);
+                                break;
                         }
                     }
                     // if no merge policy but the attribute is specified (or the config has a
                     // '*' default element), merge the values
                     else {
-                        self::merge_attribute_values($state,$name,$values);
+                        self::merge_attribute_values($state, $name, $values);
                     }
                 } else {
                     // if the attribute hasn't existed yet, let's create it
-                    self::override_attribute_values($state,$name,$values);
+                    self::override_attribute_values($state, $name, $values);
                 }
             }
             // the attribute isn't specified in the config
@@ -318,48 +327,52 @@ class attributeaggregator extends ProcessingFilter
             // but if the attribute config part is empty, we merge it.
             else {
                 if (empty($this->attributes)) {
-                    self::merge_attribute_values($state,$name,$values);
+                    self::merge_attribute_values($state, $name, $values);
                 } else {
-                    Logger::info("[attributeaggregator] Rejecting attribute ".$name);
+                    Logger::info("[attributeaggregator] Rejecting attribute " . $name);
                     continue;
                 }
             }
         }
     }
 
-    private function filter_attribute_values($name,&$values) {
+    private function filter_attribute_values($name, &$values)
+    {
         if (isset($this->attributes[$name]['values'])) {
-            $values = array_intersect($values,$this->attributes[$name]['values']);
+            $values = array_intersect($values, $this->attributes[$name]['values']);
         } elseif (isset($this->attributes['*']['values'])) {
-            $values = array_intersect($values,$this->attributes['*']['values']);
+            $values = array_intersect($values, $this->attributes['*']['values']);
         }
         // or do nothing
     }
 
-    private static function merge_attribute_values (&$state,$name,$values) {
+    private static function merge_attribute_values(&$state, $name, $values)
+    {
         if (isset($state['Attributes'][$name])) {
-            Logger::info('[attributeaggregator] Merging attribute '.$name);
-            $state['Attributes'][$name] = array_merge($state['Attributes'][$name],$values);
+            Logger::info('[attributeaggregator] Merging attribute ' . $name);
+            $state['Attributes'][$name] = array_merge($state['Attributes'][$name], $values);
         } else {
-            self::override_attribute_values($state,$name,$values);
+            self::override_attribute_values($state, $name, $values);
         }
     }
 
-    private static function override_attribute_values (&$state,$name,$values) {
+    private static function override_attribute_values(&$state, $name, $values)
+    {
         if (!empty($state['Attributes'][$name])) {
-            Logger::info('[attributeaggregator] Overriding attribute '.$name);
+            Logger::info('[attributeaggregator] Overriding attribute ' . $name);
         } else {
-            Logger::info('[attributeaggregator] Getting new attribute '.$name);
+            Logger::info('[attributeaggregator] Getting new attribute ' . $name);
         }
         $state['Attributes'][$name] = $values;
     }
 
-    private function getRequestedAttributes() {
+    private function getRequestedAttributes()
+    {
         if (empty(array_keys($this->attributes))) {
             return array();
         }
         $requestedAttributes = array_flip(array_keys($this->attributes));
-        if(array_key_exists('*',$requestedAttributes)) {
+        if (array_key_exists('*', $requestedAttributes)) {
             unset($requestedAttributes['*']);
         }
         /* Reasons for NOT including the acceptable values in the request:
@@ -367,13 +380,13 @@ class attributeaggregator extends ProcessingFilter
          *   - we can't rely on that the AA sends the matching values anyway,
          *   - in the future we might support regexp in the values array.
          */
-	
-	foreach ($requestedAttributes as $key => $value) {
-          if ($value === 0){
-	    $requestedAttributes[$key] = []; 
-          }
-	}
-	 
+
+        foreach ($requestedAttributes as $key => $value) {
+            if ($value === 0) {
+                $requestedAttributes[$key] = [];
+            }
+        }
+
         return $requestedAttributes;
     }
 
